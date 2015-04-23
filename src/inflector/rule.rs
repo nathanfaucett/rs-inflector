5dd1d64726fc67e7e1d83d372654073027d0ef2a
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-23 11:31:47 -0500

    updates

---
+++ src/inflector/rule.rs
@@ -0,0 +1,22 @@
+extern crate regex;
+use self::regex::Regex;
+
+
+pub struct Rule {
+    pub regex: Regex,
+    pub replacer: String,
+}
+
+impl Rule {
+    pub fn new(rule: String, replacer: String) -> Rule {
+        let re = match Regex::new(&rule) {
+            Ok(re) => re,
+            Err(err) => panic!("{}", err),
+        };
+
+        Rule{
+            regex: re,
+            replacer: replacer,
+        }
+    }
+}
